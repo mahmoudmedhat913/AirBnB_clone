6fa39a497ebd97310c50bf31ae5c06c7eaa6ccef
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-01-12 19:36:05 -0800

    test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -173,6 +173,7 @@ class TestAmenity_to_dict(unittest.TestCase):
         am = Amenity()
         self.assertNotEqual(am.to_dict(), am.__dict__)
 
+
     def test_to_dict_with_arg(self):
         am = Amenity()
         with self.assertRaises(TypeError):
