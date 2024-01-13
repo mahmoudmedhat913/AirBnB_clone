c6b11b2f0764ae49bde7149a54b3b7e77191b76f
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-01-13 14:19:46 -0800

    update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -21,7 +21,7 @@ class TestFileStorage_instantiation(unittest.TestCase):
     def test_FileStorage_instantiation_no_args(self):
         self.assertEqual(type(FileStorage()), FileStorage)
 
-    def test_FileStorage_instantiation_with_args(self):
+    def test_FileStorage_instantiation_with_arg(self):
         with self.assertRaises(TypeError):
             FileStorage(None)
 
