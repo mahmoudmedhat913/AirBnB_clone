63a6e6066593922bc557373cbf5864568ab995f0
omarmohammad1998 <omarelkholi4@gmail.com>
2024-01-14 11:37:50 -0800

    omar

--- test_save_reload_user.py
+++ test_save_reload_user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from models import storage
 from models.base_model import BaseModel
-rom models.user import User
+from models.user import User
 
 all_objs = storage.all()
 print("-- Reloaded objects --")
